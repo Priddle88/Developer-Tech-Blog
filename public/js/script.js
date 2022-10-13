a05182c40ff0cf2f5f2e5880034876ecce2b05b6
Parker <parkerriddle09@gmail.com>
2022-10-12 18:01:56 -0600

    Added new post button to write a new post

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,11 @@
+addPost = (event) => {
+    event.preventDefault();
+    document.querySelector('.postBox').classList.remove('gone');
+}
+
+
+
+document.getElementById('newPostBtn').addEventListener('click', addPost);
 
 console.log("THIS IS HERE");
 
